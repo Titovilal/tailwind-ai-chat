3606cfbf19ef7c180ada108c25988e85d54f0cbf
Titovilal <scastellsjuan@gmail.com>
2024-01-22 19:51:41 +0100

    Add next.js project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import Link from "next/link";
+export default function Home() {
+  return (
+    <>
+      <div>Hello world!</div>
+      <Link href="/example-route">Link a la ruta de ejemplo</Link>
+    </>
+  );
+}
