76f738b125f510daf82d51a49915ceeaccd04766
Titovilal <scastellsjuan@gmail.com>
2024-02-15 22:43:46 +0100

    Add template area for code section

--- frontend/src/app/(private)/code/page.tsx
+++ frontend/src/app/(private)/code/page.tsx
@@ -1,13 +1,34 @@
+"use client";
+import React from "react";
 import Chat from "@/components/private/Chat";
 import CodeEditor from "@/components/private/CodeEditor";
 import Renderer from "@/components/private/Renderer";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 export default function CodePage() {
   return (
     <>
-      <Chat />
-      <CodeEditor />
-      <Renderer />
+      <PanelGroup direction="horizontal">
+        <Panel>
+          <PanelGroup direction="vertical">
+            <Panel>
+              <CodeEditor />
+            </Panel>
+            <div style={{ backgroundColor: "black", height: "0.1rem" }} />
+            <PanelResizeHandle />
+            <Panel>
+              <Chat />
+            </Panel>
+          </PanelGroup>
+        </Panel>
+
+        <div style={{ backgroundColor: "black", width: "0.1rem" }} />
+        <PanelResizeHandle />
+
+        <Panel>
+          <Renderer />
+        </Panel>
+      </PanelGroup>
     </>
   );
 }
