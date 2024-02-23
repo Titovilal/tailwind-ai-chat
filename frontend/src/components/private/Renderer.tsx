361dc72e0d02594c5b451f9fe884b96eedc948c8
Nico1525 <120390023+Nico1525@users.noreply.github.com>
2024-02-23 16:59:45 +0100

    Renderer v1

--- frontend/src/components/private/Renderer.tsx
+++ frontend/src/components/private/Renderer.tsx
@@ -1,12 +1,24 @@
+import parse from 'html-react-parser';
+import DOMPurify from 'dompurify';
 
 interface RendererProps{
     postContent?: string;
 }
 const Renderer : React.FC<RendererProps> = ({postContent}) => {
+    const htmlFrom = (htmlString: string) => {
+        const cleanHtmlString = DOMPurify.sanitize(htmlString,
+          { USE_PROFILES: { html: true } });
+        const html = parse(cleanHtmlString);
+        return html;
+}
     return (
         <>
         <div className="bg-green-500">Renderer</div>
-        <label>{postContent}</label>
+        <div className="flex justify-center items-center h-full">
+            {postContent && htmlFrom(postContent)}
+        </div>
+           
+        
         </>
     );
 }
