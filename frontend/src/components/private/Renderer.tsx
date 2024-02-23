4f92faec063046e72fce3b2cbdf4cc36ec79132c
Titovilal <scastellsjuan@gmail.com>
2024-02-23 19:03:50 +0100

    Enable TailwindCSS when rendering code

--- frontend/src/components/private/Renderer.tsx
+++ frontend/src/components/private/Renderer.tsx
@@ -1,25 +1,34 @@
-import parse from 'html-react-parser';
-import DOMPurify from 'dompurify';
+import parse from "html-react-parser";
+import DOMPurify from "dompurify";
 
-interface RendererProps{
-    postContent?: string;
+interface RendererProps {
+  postContent?: string;
 }
-const Renderer : React.FC<RendererProps> = ({postContent}) => {
-    const htmlFrom = (htmlString: string) => {
-        const cleanHtmlString = DOMPurify.sanitize(htmlString,
-          { USE_PROFILES: { html: true } });
-        const html = parse(cleanHtmlString);
-        return html;
-}
-    return (
-        <>
-        <div className="bg-green-500">Renderer</div>
-        <div className="flex justify-center items-center h-full">
-            {postContent && htmlFrom(postContent)}
-        </div>
-           
-        
-        </>
-    );
-}
-export default Renderer; 
+
+const tailwind = `<style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css');
+</style>`;
+
+const Renderer: React.FC<RendererProps> = ({ postContent }) => {
+  const htmlFrom = (htmlString: string) => {
+    const cleanHtmlString = DOMPurify.sanitize(htmlString, {
+      USE_PROFILES: { html: true },
+    });
+    const html = parse(cleanHtmlString);
+    return html;
+  };
+
+  const setTailwindCSS = (htmlString: string) => {
+    return { __html: htmlString };
+  };
+  return (
+    <>
+      <div className="bg-green-500">Renderer</div>
+      <div className="flex justify-center items-center h-full w-full">
+        <div dangerouslySetInnerHTML={setTailwindCSS(tailwind)} />
+        {postContent && htmlFrom(postContent)}
+      </div>
+    </>
+  );
+};
+export default Renderer;
