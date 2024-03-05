3d800b1f429dca3d2027eafda2770e21172c6d7d
Titovilal <scastellsjuan@gmail.com>
2024-03-05 20:47:07 +0100

    Fix bugs

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from routers.code import router_code
+
 app = FastAPI(
     title="Tailwind AI Chatbot API",
     description="",
     version="0.1.0",
 )
 
-app.include_router(router_regex)
-app.include_router(router_adverse_factors)
+app.include_router(router_code)
 
 if __name__ == "__main__":
 
