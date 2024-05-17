62a4c81fed43a789de7275b21c803c4aecc8d4af
Titovilal <scastellsjuan@gmail.com>
2024-05-17 19:05:04 +0200

    add cors

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.chat import router_chat
 from routers.message import router_message
-from routers.user import router_user
+from routers.account import router_account
 
 app = FastAPI(
     title="Tailwind AI Chatbot API",
@@ -11,9 +12,17 @@ app = FastAPI(
     version="0.1.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router_chat)
 app.include_router(router_message)
-app.include_router(router_user)
+app.include_router(router_account)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8001)
