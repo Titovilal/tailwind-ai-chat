c21b0ac022c2316e2c5cdc121050a3fd41a47639
Titovilal <scastellsjuan@gmail.com>
2024-05-18 20:18:48 +0200

    remove useless div

--- frontend/components/main-nav.tsx
+++ frontend/components/main-nav.tsx
@@ -19,52 +19,51 @@ import Link from "next/link";
 const MainNav = () => {
   return (
     <nav className="flex flex-col justify-between items-center bg-background border-r pt-2 pb-4">
-      <div className="space-y-4">
-        <section className="grid gap-4">
-          <Link
-            href="/"
-            className="group flex h-9 w-9 items-center justify-center rounded-lg text-background bg-primary transition-colors"
-          >
-            <Bot className="h-5 w-5 group-hover:scale-110" />
-          </Link>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="code"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-primary"
-              >
-                <Code className="h-5 w-5" />
-                <span className="sr-only">Code</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Code</TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="history"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-primary"
-              >
-                <History className="h-5 w-5" />
-                <span className="sr-only">History</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">History</TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="questions"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-primary"
-              >
-                <MessageCircleQuestion className="h-5 w-5" />
-                <span className="sr-only">Questions</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Questions</TooltipContent>
-          </Tooltip>
-        </section>
-      </div>
+      <section className="grid gap-4">
+        <Link
+          href="/"
+          className="group flex h-9 w-9 items-center justify-center rounded-lg text-background bg-primary transition-colors"
+        >
+          <Bot className="h-5 w-5 group-hover:scale-110" />
+        </Link>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="code"
+              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-primary"
+            >
+              <Code className="h-5 w-5" />
+              <span className="sr-only">Code</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">Code</TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="history"
+              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-primary"
+            >
+              <History className="h-5 w-5" />
+              <span className="sr-only">History</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">History</TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="questions"
+              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-primary"
+            >
+              <MessageCircleQuestion className="h-5 w-5" />
+              <span className="sr-only">Questions</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">Questions</TooltipContent>
+        </Tooltip>
+      </section>
+
       <section className="grid gap-4">
         <Tooltip>
           <TooltipTrigger asChild>
