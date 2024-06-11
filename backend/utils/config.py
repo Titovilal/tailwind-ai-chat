af988ecb6ce7f951e8f07988a019f8314f66c3ba
Nico1525 <120390023+Nico1525@users.noreply.github.com>
2024-06-11 20:33:32 +0200

    Prompts mejorados

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -15,16 +15,18 @@ openai_client = OpenAI(api_key=os.environ.get("OPENAI_KEY"))
 def chat_gpt(
     message: str,
     client: OpenAI = openai_client,
-    role: str = "You are an assistant that writes Tailwind code. Your response should be in JSON format, with the following keys: explanation, code. In the explanation i want only text that explains the code, and in code i want only the tailwind code",
+    role: str = "You are an assistant that writes Tailwind code. Your response should be in JSON format, with the following keys: explanation, code. In the explanation i want only text that explains the code, and in code i want only the tailwind code." ,
     model: str = "gpt-3.5-turbo",
 ) -> str:
-    completion = client.chat.completions.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": role},
-            {"role": "user", "content": message},
-        ],
-    )
-
-    return completion.choices[0].message.content
+    try:
+        completion = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": role},
+                {"role": "user", "content": message},
+            ],
+        )
+        return completion.choices[0].message.content
+    except Exception as e:
+        return f"An error occurred: {e}"
 
