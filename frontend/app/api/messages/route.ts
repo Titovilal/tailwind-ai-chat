87f1721187ac70761b20993580fc9020cbf73534
Titovilal <scastellsjuan@gmail.com>
2024-06-19 18:42:33 +0200

    Update messages post api call

--- frontend/app/api/messages/route.ts
+++ frontend/app/api/messages/route.ts
@@ -6,6 +6,36 @@ export async function GET() {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  console.log(body);
+
+  // Crear mensaje de usuario y mandarlo a la base de datos
+  const userMessage: UserMessage = body; // TODO Comprobar
+
+  // Guardar mensaje de usuaroi en la BBDD
+  // TODO
+
+  // Crear prompt dependiendo de si se usa el código
+  // TODO
+
+  // Hacer llamada a API GPT
+  // TODO
+
+  // Cargar respuesta en EXPLANATION y CODE
+  // TODO
+
+  // Crear AIMessage con los valores de EXPLANATION y CODE
+  // TODO
+
+  // Guardar AIMessage en la BBDD
+  // TODO
+
+  // Crear objeto QA
+  // TODO
+
+  // Guardar QA en la BBDD
+  // TODO
+
+  // Devolver objeto AIMessageResponse
+  // TODO
+
   return NextResponse.json({ status: "200", message: "OK" });
 }
